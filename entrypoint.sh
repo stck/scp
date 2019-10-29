3a36ed89f60d1a7f48d941ec522b0a57d55cf951
Ivan Okunev <stck@users.noreply.github.com>
2019-10-29 15:59:20 +0300

    fix input source

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,20 @@
 
 echo "${INPUT_KEY}" > key
 if [[ -f $INPUT_LOCAL ]]; then
-  FILE_ARGS="-p ${INPUT_LOCAL}"
+  FILE_ARGS="-p ./${INPUT_LOCAL}"
 elif [[ -d $INPUT_LOCAL ]]; then
-  FILE_ARGS="-rp ${INPUT_LOCAL}"
-elif
+  FILE_ARGS="-rp ./${INPUT_LOCAL}"
+else
   return 1;
 fi;
 
+echo "${FILE_ARGS}"
+
 chmod 600 key
 
-scp -o StrictHostKeyChecking=no -v -i ./key -P "${INPUT_PORT}" "${FILE_ARGS}" "${INPUT_USER}"@"${INPUT_HOST}":"${INPUT_REMOTE}"
+scp -o StrictHostKeyChecking=no -v -i ./key -P $INPUT_PORT $FILE_ARGS $INPUT_USER@$INPUT_HOST:$INPUT_REMOTE
 if [ ! -z "${INPUT_COMMAND}" ]; then
-  ssh -o StrictHostKeyChecking=no -v -i ./key -p "${INPUT_PORT}" "${INPUT_USER}"@"${INPUT_HOST}" "${INPUT_COMMAND}"
+  echo "ssh -o StrictHostKeyChecking=no -v -i ./key -p $INPUT_PORT $INPUT_USER@$INPUT_HOST '${INPUT_COMMAND}'"
 fi;
 
 rm -rf ./key
