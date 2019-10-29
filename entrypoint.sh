8e96ae6dbfe214e126e863ad2837e92ffcb81df7
Ivan Okunev <stck@users.noreply.github.com>
2019-10-29 15:53:28 +0300

    fix shebang

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-# shellcheck disable=SC2039
-echo -e "${INPUT_KEY}" >__TEMP_INPUT_KEY_FILE
+echo "${INPUT_KEY}" > key
+if [[ -f $INPUT_LOCAL ]]; then
+  FILE_ARGS="-p ${INPUT_LOCAL}"
+elif [[ -d $INPUT_LOCAL ]]; then
+  FILE_ARGS="-rp ${INPUT_LOCAL}"
+elif
+  return 1;
+fi;
 
-chmod 600 __TEMP_INPUT_KEY_FILE
+chmod 600 key
 
-scp -o StrictHostKeyChecking=no -v -i __TEMP_INPUT_KEY_FILE -P "${INPUT_PORT}" -r ${INPUT_LOCAL} "${INPUT_USER}"@"${INPUT_HOST}":"${INPUT_REMOTE}"
+scp -o StrictHostKeyChecking=no -v -i ./key -P "${INPUT_PORT}" "${FILE_ARGS}" "${INPUT_USER}"@"${INPUT_HOST}":"${INPUT_REMOTE}"
+if [ ! -z "${INPUT_COMMAND}" ]; then
+  ssh -o StrictHostKeyChecking=no -v -i ./key -p "${INPUT_PORT}" "${INPUT_USER}"@"${INPUT_HOST}" "${INPUT_COMMAND}"
+fi;
+
+rm -rf ./key
