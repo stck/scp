9df709fcb5d2e1d27caf425c620d578167671780
Ivan Okunev <stck@users.noreply.github.com>
2020-03-14 22:33:30 +0300

    add precopy command

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,9 @@ fi;
 echo "${FILE_ARGS}"
 
 chmod 600 key
-
+if [ ! -z "${INPUT_PRECOPY}" ]; then
+  echo "ssh -o StrictHostKeyChecking=no -v -i ./key -p $INPUT_PORT $INPUT_USER@$INPUT_HOST '${INPUT_PRECOPY}'"
+fi;
 scp -o StrictHostKeyChecking=no -v -i ./key -P $INPUT_PORT $FILE_ARGS $INPUT_USER@$INPUT_HOST:$INPUT_REMOTE
 if [ ! -z "${INPUT_COMMAND}" ]; then
   echo "ssh -o StrictHostKeyChecking=no -v -i ./key -p $INPUT_PORT $INPUT_USER@$INPUT_HOST '${INPUT_COMMAND}'"
